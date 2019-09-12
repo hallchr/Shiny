621e60f0de4027510551e86415f611bd82a5fd45
Chris Hall <chris.hall@bothellwa.gov>
2019-09-12 11:47:25 -0700

    Continuous temp monitoring

--- Shiny-R_ContTEMP.R
+++ Shiny-R_ContTEMP.R
@@ -35,54 +35,26 @@ library(dplyr)
 library(readr)
 library(rsconnect)
 
-dbPath1 <- "TMDL_Stream_2010-2019_V3.xlsx"
-
-# load the workbook
-
-wb <- loadWorkbook(dbPath1)
+#path of spreadsheet
+dbPath1 <- "All_Loggers.xlsx"
 
 #load the worksheets
-SARU_Excel <-  read.xlsx(dbPath1, "SARU", detectDates = T)
-LS_1_Excel <-  read.xlsx(dbPath1, "LS_1", detectDates = T)
-JO_1_Excel <-  read.xlsx(dbPath1, "JO_1", detectDates = T)
-PM_1_Excel <-  read.xlsx(dbPath1, "PM_1", detectDates = T)
-NCLD_Excel <-  read.xlsx(dbPath1, "NCLD", detectDates = T)
-HC_1_Excel <-  read.xlsx(dbPath1, "HC_1", detectDates = T)
-HC_2_Excel <-  read.xlsx(dbPath1, "HC_2", detectDates = T)
-NC_1_Excel <-  read.xlsx(dbPath1, "NC_1", detectDates = T)
-WC_1_Excel <-  read.xlsx(dbPath1, "WC_1", detectDates = T)
-QC_1_Excel <- read.xlsx(dbPath1, "QC_1", detectDates = T)
-NC_2_Excel <- read.xlsx(dbPath1, "NC_2", detectDates = T)
-PR_2_Excel <- read.xlsx(dbPath1, "PR_2", detectDates = T)
-MD_1_Excel <- read.xlsx(dbPath1, "MD_1", detectDates = T)
-WD_1_Excel <- read.xlsx(dbPath1, "WD_1", detectDates = T)
-PA_1_Excel <- read.xlsx(dbPath1, "PA_1", detectDates = T)
-PA_2_Excel <- read.xlsx(dbPath1, "PA_2", detectDates = T)
-BY_1_Excel <- read.xlsx(dbPath1, "BY_1", detectDates = T)
-MH_1_Excel <- read.xlsx(dbPath1, "MH_1", detectDates = T)
-# Combine Worksheets
-AllCountsAmbient <- rbind(SARU_Excel, LS_1_Excel, JO_1_Excel, PM_1_Excel,
-                          NCLD_Excel, HC_1_Excel,HC_2_Excel, NC_1_Excel, WC_1_Excel, QC_1_Excel,
-                          NC_2_Excel, PR_2_Excel, MD_1_Excel, WD_1_Excel, PA_1_Excel, PA_2_Excel, BY_1_Excel, MH_1_Excel)
-#Creat Year and Month Vectors
-
-AllCountsAmbient$Month <- month(AllCountsAmbient$Date, label = TRUE, abbr = TRUE)
-
-#get objects in correct form
-AllCountsAmbient$pH <- as.numeric(AllCountsAmbient$pH)
-AllCountsAmbient$Turbidity <- as.numeric(AllCountsAmbient$Turbidity)
-
-AllCountsAmbient$Temp.Type <- ifelse(AllCountsAmbient$TEMP < 16, "below", "above")
-AllCountsAmbient$DO.Type <- ifelse(AllCountsAmbient$DO > 9.5, "below", "above")
-AllCountsAmbient$Turbidity.Type <- ifelse(AllCountsAmbient$Turbidity < 4, "below", "above")
-
-AllCountsAmbient2019 <- subset(AllCountsAmbient, Year %in% "2019")
-AllCountsAmbient2018 <- subset(AllCountsAmbient, Year %in% "2018")
-AllCountsAmbient2018_2019 <- subset(AllCountsAmbient, Year %in% c("2018", "2019"))
+NC <-  read.xlsx(dbPath1, "NC", detectDates = T)
+HC <-  read.xlsx(dbPath1, "HC", detectDates = T)
+PA <-  read.xlsx(dbPath1, "PA", detectDates = T)
+WC <-  read.xlsx(dbPath1, "WC", detectDates = T)
 
 
-AllCountsAmbient$CurrentYear <- '2010-2018'
-AllCountsAmbient$CurrentYear[AllCountsAmbient$Year == '2019'] <- '2019'
+#combine worksheets
+
+AllCounts<- rbind(NC, HC, PA, WC)
+
+#add logical for above 16
+
+AllCounts$Temperature.Above.16 <- AllCounts$Seven_DADMax>16
+AllCounts$Temp.Type <- ifelse(AllCounts$Seven_DADMax < 16, "below", "above")
+
+nms1 <- names(AllCounts)
 
 f <- list(
   family = "helvetica",
@@ -98,22 +70,20 @@ x <- list(
   titlefont = f
 )
 
-AllCountsAmbient3 <- AllCountsAmbient[, c("TEMP", "DO", "Site", "Specific.Cond", "Turbidity", "Fecal.Coliform", "Year", "Precipitation", "BIBI", "Cu", "Zn", "Pb", "TN", "TP")]
-nms3 <- names(AllCountsAmbient3)
 
 ui <- fluidPage(
   
-  headerPanel("Bothell Water Quality"),
+  headerPanel("Bothell Continuous Temperature Loggers"),
   sidebarPanel(
-    sliderInput('sampleSize', 'Sample Size', min = 1, max = nrow(AllCountsAmbient3),
-                value = 1000, step = 50, round = 0),
-    selectInput('x', 'X', choices = "Site", selected = "Site"),
-    selectInput('y', 'Y', choices = c("TEMP", "DO", "Specific.Cond", "Turbidity", "TSS", "Fecal.Coliform", "BIBI", "Cu", "Zn", "Pb", "TN", "TP"), selected = "TEMP"),
-    selectInput('color', 'Color', choices = c("Site", "Year", "Precipitation"), selected = "Site"),
+    sliderInput('sampleSize', 'Sample Size', min = 1, max = nrow(AllCounts),
+                value = 10000, step = 50, round = 0),
+    selectInput('x', 'X', choices = "Date", selected = "Date"),
+    selectInput('y', 'Y', choices = "Seven_DADMax", selected = "Seven_DadMax"),
+    selectInput('color', 'Color', choices = "Temp.Type", selected = "Temp.Type"),
     
     # selectInput('facet_row', 'Facet Row', c(None = '.', nms2), selected = "Site"), to choose all categories as facet type
-    selectInput('facet_row', 'Facet Row', c(None = '.', "Site")),
-    selectInput('facet_col', 'Facet Column', c(None = '.', "Precipitation")),
+    selectInput('facet_row', 'Facet Row', choices = "Site",, selected = "Site"),
+    selectInput('facet_col', 'Facet Column', c(None = '.', "Site")),
     sliderInput('plotHeight', 'Height of plot (in pixels)', 
                 min = 100, max = 1000, value = 700)
   ),
@@ -128,14 +98,14 @@ server <- function(input, output) {
   
   #add reactive data information. Dataset = built in diamonds data
   dataset <- reactive({
-    AllCountsAmbient3[sample(nrow(AllCountsAmbient3), input$sampleSize),]
+    AllCounts[sample(nrow(AllCounts), input$sampleSize),]
   })
   
   output$trendPlot <- renderPlotly({
     
     # build graph with ggplot syntax
     p <- ggplot(dataset(), aes_string(x = input$x, y = input$y, color = input$color)) + 
-      geom_boxplot() +theme_bw() + theme(axis.title.y = element_text(size=18), title = element_text(size = 18), axis.text.x = element_text(size = 12, angle = 65, vjust = 0.6),
+      geom_point() + geom_hline(yintercept = 16, color = "red") + theme_bw() + theme(axis.title.y = element_text(size=18), title = element_text(size = 18), axis.text.x = element_text(size = 12, angle = 65, vjust = 0.6),
                                          axis.text.y = element_text(size = 15), legend.title = element_text(size=15), legend.text = element_text(size=15), legend.position = "none")
     
     # if at least one facet column/row is specified, add it
